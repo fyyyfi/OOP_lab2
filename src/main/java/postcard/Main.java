ba9635828dbb08a43cb26d9c29b11c9ecd8c9113
fyyyfi <katyuha.mik@gmail.com>
2025-10-09 14:47:25 +0300

    redacted

--- src/main/java/postcard/Main.java
+++ src/main/java/postcard/Main.java
@@ -17,18 +17,16 @@ import java.util.List;
 public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final String XML_FILE_PATH = "src/main/resources/old_cards.xml";
+    private static final String XSD_FILE_PATH = "src/main/resources/old_cards.xsd";
+    private static final String XSLT_FILE_PATH = "src/main/resources/old_cards.xsl";
+    private static final String OUTPUT_XML_FILE_PATH = "grouped_by_country.xml";
 
     public static void main(String[] args) {
-        logger.info("--- Запуск програми 'Старі листівки' ---");
+        logger.info("Запуск програми 'Старі листівки'");
 
-        String xmlFile = "src/main/resources/old_cards.xml";
-        String xsdFile = "src/main/resources/old_cards.xsd";
-        String xsltFile = "src/main/resources/old_cards.xsl";
-        String outputXmlFile = "grouped_by_country.xml";
-
-  
-        logger.info("Крок 1: Валідація файлу {}", xmlFile);
-        if (XmlValidator.validate(xmlFile, xsdFile)) {
+        logger.info("Крок 1: Валідація файлу {}",XML_FILE_PATH);
+        if (XmlValidator.validate(XML_FILE_PATH, XSD_FILE_PATH)) {
             logger.info("Результат: XML є валідним.");
         } else {
             logger.error("Результат: XML не є валідним. Роботу програми зупинено.");
@@ -41,7 +39,7 @@ public class Main {
 
         try {
             logger.info(" Використання DOM парсера");
-            List<OldCard> cards = new DomCardParser().parse(xmlFile);
+            List<OldCard> cards = new DomCardParser().parse(XML_FILE_PATH);
             logger.info("Знайдено {} листівок", cards.size());
              cards.sort(CardComparators.byYear());
             logger.info("Листівки, відсортовані за роком:");
@@ -52,7 +50,7 @@ public class Main {
 
         try {
             logger.info("Використання SAX парсера");
-            List<OldCard> cards = new SaxCardParser().parse(xmlFile);
+            List<OldCard> cards = new SaxCardParser().parse(XML_FILE_PATH);
             logger.info("Знайдено {} листівок", cards.size());
             cards.sort(CardComparators.byCountry());
             logger.info("Листівки, відсортовані за країною:");
@@ -63,7 +61,7 @@ public class Main {
 
         try {
             logger.info("Використання STAX парсера");
-            List<OldCard> cards = new StaxCardParser().parse(xmlFile);
+            List<OldCard> cards = new StaxCardParser().parse(XML_FILE_PATH);
             logger.info("Знайдено {} листівок.", cards.size());
             cards.sort(CardComparators.byId());
             logger.info("Листівки, відсортовані за ID:");
@@ -73,7 +71,7 @@ public class Main {
         }
 
         logger.info("Крок 3: XSLT трансформація");
-        XsltTransformer.transform(xmlFile, xsltFile, outputXmlFile);
-        logger.info("Трансформацію завершено. Результат у файлі: {}", outputXmlFile);
+        XsltTransformer.transform(XML_FILE_PATH, XSLT_FILE_PATH, OUTPUT_XML_FILE_PATH);
+        logger.info("Трансформацію завершено. Результат у файлі: {}", OUTPUT_XML_FILE_PATH);
     }
 }
